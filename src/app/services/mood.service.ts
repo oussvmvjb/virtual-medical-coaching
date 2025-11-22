26c587691d60993dc25850687c396e519ef189ce
chahine <chahine.abdellii@gmail.com>
2025-11-22 21:16:25 +0100

    services

---
+++ src/app/services/mood.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Evaluation } from '../models/evaluation.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EvaluationService { 
+  private apiUrl = 'http://localhost:8080/api/evaluations'; 
+
+  constructor(private http: HttpClient) { }
+
+  createEvaluation(evaluation: Evaluation): Observable<Evaluation> {
+    return this.http.post<Evaluation>(this.apiUrl, evaluation);
+  }
+}
