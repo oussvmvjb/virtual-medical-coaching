bb7a662a91eebcbb280ffe7f0c9988a3123e26dc
oussema136 <oussemaa136@gmail.com>
2025-11-22 20:01:59 +0100

    role

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -3,24 +3,24 @@ export interface User {
   nom: string;
   prenom: string;
   email: string;
-  tel: string;
+  telephone: string;
   psw: string;
+  
 }
 
 export interface LoginRequest {
   email: string;
   psw: string;
 }
-
 export interface SignupRequest {
   nom: string;
   prenom: string;
   email: string;
-  tel: string;
+  telephone: string; // Changé de 'tel' à 'telephone'
   psw: string;
   confirmPassword: string;
+  role: string; // Ajout du champ rôle
 }
-
 export interface ForgotPasswordRequest {
   email: string;
 }
