dfb7f4e521c9f6f7e83868fa48c7d502823d9786
chahine <chahine.abdellii@gmail.com>
2025-11-24 18:18:16 +0100

    list

--- src/app/components/list-evaluation/list-evaluation.component.ts
+++ src/app/components/list-evaluation/list-evaluation.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EvaluationService } from '../../services/mood.service';
-import { Router } from '@angular/router';   // <-- Ajouté
+import { Router } from '@angular/router';   
 
 @Component({
   selector: 'app-evaluation-humeurs',
